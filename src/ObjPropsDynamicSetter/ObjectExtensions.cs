1946ccbaf432cb94390d5055b4c091ba64ac43b1
Eugene Klimeshuk <17860027+Molnix888@users.noreply.github.com>
2021-03-14 16:37:46 +0100

    Suppress non-relevant warning

--- src/ObjPropsDynamicSetter/ObjectExtensions.cs
+++ src/ObjPropsDynamicSetter/ObjectExtensions.cs
@@ -139,7 +139,9 @@ namespace ObjPropsDynamicSetter
 
             if (includeNonPublic)
             {
+#pragma warning disable S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
                 flags |= BindingFlags.NonPublic;
+#pragma warning restore S3011 // Reflection should not be used to increase accessibility of classes, methods, or fields
             }
 
             var type = obj.GetType();
